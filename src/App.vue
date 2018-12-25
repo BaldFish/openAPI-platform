c3b5c4c21cbe81265aa9a3450c7c9673dddce040
zhangzhenchang <591781589@qq.com>
2018-12-25 19:26:58 +0800

    登录 nav

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="app">
-    <div class="head-wrap" ref="head">
+    <div class="head-wrap" ref="head" v-if="isShowTopSearch">
       <div class="head">
-        <img src="@/common/images/logo_header.png" alt="">
+        <span>区块链接口开放平台</span>
         <ul class="">
           <li class="nav">
-            <span></span>
             <router-link to="/home">首页</router-link>
           </li>
           <li class="nav">
-            <span></span>
             <router-link to="/apiList">接口统计</router-link>
           </li>
           <li class="nav">
-            <span></span>
             <router-link to="/ability">能力库</router-link>
           </li>
         </ul>
+        <div class="no_login" v-if="!isLogin">
+          <a href="javascript:void(0)" @click="login">登录</a>
+        </div>
+        <div class="login" v-if="isLogin">
+          <div @click.stop="toggle">
+            <img src="@/common/images/bg.jpg" class="user-head" alt="">
+            admin&nbsp;
+            <img src="@/common/images/down.png" alt="">
+          </div>
+          <ul v-if="switchover" @mouseleave.stop="leaveUl">
+            <li><a href="" target="_blank">账户信息</a></li>
+            <li @click.stop="dropOut">退出</li>
+          </ul>
+        </div>
       </div>
     </div>
     <router-view class="main"></router-view>
@@ -29,15 +40,56 @@
     components: {},
     data() {
       return {
+        isShowTopSearch: true,
+        isLogin: false,
+        switchover: false,
       }
     },
     created() {
     },
     mounted() {
+      this.changTop()
     },
     watch: {},
     computed: {},
     methods: {
+      changTop() {
+       if (this.$route.path == "/login") {
+         this.isShowTopSearch = false;
+       }
+      },
+      toggle() {
+        this.switchover = !this.switchover
+      },
+      leaveUl() {
+        this.switchover = false
+      },
+      login() {
+        let redirectURL = window.location.href;
+        let url=`?redirectURL=${redirectURL}`;
+        window.location.href=`${loginPlatform}/login${url}`;
+      },
+      dropOut() {
+        let sessionsId = JSON.parse(sessionStorage.getItem("userInfo")).session_id;
+        this.$axios({
+          method: 'DELETE',
+          url: `${this.$baseURL}/v1/sessions/${sessionsId}`,
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          }
+        }).then(res => {
+          sessionStorage.removeItem('loginInfo');
+          sessionStorage.removeItem('userInfo');
+          /*document.cookie = `token=;expires=${new Date(0)}`;
+          document.cookie = `user_id=;expires=${new Date(0)}`;*/
+          document.cookie = `token=;expires=${new Date(0)};domain=.datajs.com.cn`;
+          document.cookie = `user_id=;expires=${new Date(0)};domain=.datajs.com.cn`;
+          this.switchover = false;
+          location.reload()
+        }).catch(error => {
+          console.log(error);
+        })
+      },
     },
   };
 </script>
@@ -48,50 +100,46 @@
     display: flex;
     flex-direction: column;
   }
-  
+
   .head-wrap {
     position: absolute;
     z-index: 1000;
     width: 100%;
     height: 100px;
-    background-color: #ffffff;
+    background-color: #2e3644;
     .head {
       position relative
       box-sizing: border-box
       width: 1200px;
+      height: 100px;
       margin: 0 auto;
       padding 4px 35px
-      font-size 0
-      img {
-        vertical-align: top
+      font-size 20px;
+      color #ffffff
+      span{
+        font-weight 700
+        position: relative;
+        top: 6px;
       }
       ul {
-        width 919px
+        width 720px
         display inline-block
         text-align right
         margin-top 14px
         .nav {
           display: inline-block;
           margin-right 70px
-          font-size 0px
+          font-size: 16px;
           box-sizing: border-box
-          span {
-            display inline-block
-            width: 4px;
-            height: 4px;
-            background-color: #333333;
-            border-radius 2px
-            margin-right 8px
-          }
           & > a {
             display: inline-block;
-            font-size 14px
-            color #333333
+            font-size: 16px;
+            color #ffffff
             box-sizing border-box
             line-height 54px
             &.router-link-active, &:hover {
-              color: #333333;
-              border-bottom 1px solid #5226f3
+              color #ffffff
+              border-bottom 1px solid #ffffff
             }
           }
         }
@@ -99,9 +147,68 @@
           margin-right 0
         }
       }
+      .no_login {
+        vertical-align top
+        display inline-block
+        margin-right 20px
+        margin-left 58px
+        width 140px
+        padding-top: 29px;
+        a {
+          font-size 16px
+          margin-left 28px
+          color: #ffffff;
+        }
+      }
+      .login {
+        display inline-block
+        cursor pointer
+        margin-right 20px
+        margin-left 58px
+        width 140px
+        position relative
+        color #ffffff
+        font-size 16px
+        img {
+          width: 13px;
+          height: 9px;
+        }
+        .user-head{
+          width: 28px;
+          height: 28px;
+          border-radius 50%
+          position: relative;
+          top: 8px;
+          margin-right: 5px;
+        }
+        ul {
+          background-color: #fff;
+          position: absolute;
+          top: 64px;
+          right: 20px;
+          text-align: center;
+          width: 130px;
+          font-size: 16px;
+          box-shadow: 0 5px 15px 0 rgba(175, 192, 209, 0.55);
+          li {
+            height 40px
+            line-height 40px
+            color #333333
+            a {
+              color #333333
+            }
+          }
+          li:hover {
+            color #1d79fe
+            a {
+              color #1d79fe
+            }
+          }
+        }
+      }
     }
   }
-  
+
   .main {
     flex: 1;
     box-sizing: border-box;
@@ -109,5 +216,5 @@
     width 100%
     min-width 1200px
   }
-  
+
 </style>
--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,29 @@
 <template>
   <div class="login">
-    login
+    <div class="login-container">
+      <div class="content">
+        <p class="title">区块链接口开放平台</p>
+        <ul>
+          <li>
+            <i></i>
+            <input type="text" placeholder="请输入用户名">
+          </li>
+          <li>
+            <i></i>
+            <input type="text" placeholder="请输入密码">
+          </li>
+          <li>
+            <i></i>
+            <input type="text" placeholder="请输入验证码">
+            <img @click="getCaptcha" :src="captcha_number" alt="">
+          </li>
+        </ul>
+        <p class="checkError">密码输入错误</p>
+        <router-link to="">
+          <p class="login-btn">登录</p>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,21 +32,119 @@
     name: "login",
     components: {},
     data() {
-      return {}
+      return {
+        captcha_number: "",
+        captcha_id: "",
+      }
     },
     created() {
     },
     beforeMount() {
     },
     mounted() {
+      this.$nextTick(() => {
+        this.getCaptcha()
+      });
     },
     watch: {},
     computed: {},
-    methods: {},
+    methods: {
+      //获取图片验证码
+      getCaptcha() {
+        this.$axios({
+          method: 'post',
+          url: `${this.$baseURL}/v1/captcha`,
+          data: this.$querystring.stringify({})
+        }).then(res => {
+          this.captcha_number = `data:image/png;base64,${res.data.png}`;
+          this.captcha_id = res.data.captcha_id;
+        }).catch(error => {
+          console.log(error);
+        });
+      },
+    },
   }
 </script>
 
 <style scoped lang="stylus">
   .login {
+    width: 100%
+    height: 100%
+    background: url("../../common/images/bg.jpg") no-repeat center
+    background-size 100% 100%
+    .login-container{
+      width: 464px;
+      height: 421px;
+      background-color: #ffffff;
+      border-radius: 30px;
+      margin-top 12%
+      margin-left 48%
+      .content{
+        width:326px
+        margin 0 auto
+        padding-top: 50px;
+        .title{
+          font-size: 24px;
+          color: #333333;
+          text-align center
+          margin-bottom 55px
+        }
+        ul{
+          li{
+            font-size: 14px;
+            border-bottom: solid 1px #cecece;
+            padding-bottom: 13px;
+            margin-top: 13px;
+            i{
+              margin-right 12px
+              position: relative
+              top:3px
+            }
+            img{
+              width: 91px;
+              height: 32px;
+              float right
+              margin-right 6px
+            }
+          }
+          li:nth-child(1) i{
+            width: 16px;
+            height: 18px;
+            display inline-block
+            background url("../../common/images/icon_user.png") no-repeat center
+            background-size 100% 100%
+          }
+          li:nth-child(2) i{
+            width: 16px;
+            height: 22px;
+            display inline-block
+            background url("../../common/images/iicon_password.png") no-repeat center
+            background-size 100% 100%
+          }
+          li:nth-child(3) i{
+            width: 16px;
+            height: 18px;
+            display inline-block
+            background url("../../common/images/icon_code.png") no-repeat center
+            background-size 100% 100%
+          }
+        }
+        .checkError{
+          font-size: 14px;
+          color: #e60000;
+          margin:14px 0 22px 0
+        }
+        .login-btn{
+          width: 326px;
+          height: 40px
+          background-color: #1d79fe;
+          border-radius: 20px;
+          line-height 40px
+          text-align center
+          color #ffffff
+          font-size: 18px;
+        }
+      }
+    }
   }
 </style>
